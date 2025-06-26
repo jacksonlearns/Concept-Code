706bbdb928cafa04f17a88632566bb8a368d5a6d
jacksonlearns <135264524+jacksonlearns@users.noreply.github.com>
2025-06-26 16:17:28 -0400

    Add files via upload

---
+++ cal-add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x, y;
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    std::cout << "Enter another number: ";
+    std::cin >> y;
+    std::cout << "The sum is: " << x + y << "\n";
+    return 0;
+}
